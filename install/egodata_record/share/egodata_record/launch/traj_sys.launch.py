9d9b63058fda952cad0111d39055abf7aa08ffa5
shi-akihi <akihi_sjtu@sjtu.edu.cn>
2025-11-26 22:51:38 +0800

    minor: remove ignored files

--- install/egodata_record/share/egodata_record/launch/traj_sys.launch.py
+++
@@ -1,9 +0,0 @@
-from launch import LaunchDescription
-from launch_ros.actions import Node
-
-def generate_launch_description():
-    return LaunchDescription([
-        Node(package='egodata_record', executable='udp_listener', output='screen'),
-        Node(package='egodata_record', executable='zed_tracker', output='screen'),
-        Node(package='egodata_record', executable='zed_xreal_traj_recorder', output='screen'),
-    ])
